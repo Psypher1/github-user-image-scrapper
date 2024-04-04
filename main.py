408ecdc6cccbea77878b71d744dcb8e8688499b2
dante <jamesmidzi@gmail.com>
2024-04-04 15:38:42 +0200

    fetching working

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ def scrape_image():
     print("one:", github_url)
     print("two:", full_url)
 
+    request = requests.get(full_url)
+    soup = bs(request.content, "html.parser")
+    profile_image = soup.find("img")["src"]
+    print(profile_image)
+
 
 if __name__ == "__main__":
     scrape_image()
