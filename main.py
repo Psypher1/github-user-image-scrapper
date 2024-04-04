c7a5c1e3d39feca6704ef7943a5ad968c74b48cf
dante <jamesmidzi@gmail.com>
2024-04-04 15:23:46 +0200

    url fetched

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+
 def scrape_image():
-    pass
+    github_user = input("Enter User Handle:")
+    github_url = "https://github.com/"
+    full_url = f"{github_url}{github_user}"
+
+    print("one:", github_url)
+    print("two:", full_url)
 
 
 if __name__ == "__main__":
